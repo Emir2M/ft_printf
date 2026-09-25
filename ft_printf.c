c83850fff6d2f1f1ce5e526e2760882278b0ba45
Emirhan Yildirim <emirhyil@k1m15s08.42istanbul.com.tr>
2026-09-25 14:53:05 +0300

    refactor_v3: final check and formatting

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: emirhyil <emirhyil@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/23 16:12:29 by emirhyil          #+#    #+#             */
-/*   Updated: 2026/09/25 14:44:02 by emirhyil         ###   ########.fr       */
+/*   Updated: 2026/09/25 14:52:30 by emirhyil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	ft_printf(const char *string, ...)
 
 	i = 0;
 	len = 0;
-	if (string[i] == NULL)
+	if (!string)
 		return (-1);
 	va_start(ap, string);
 	while (string[i] != '\0')
